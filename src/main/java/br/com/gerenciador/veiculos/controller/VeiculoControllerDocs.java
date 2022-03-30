0d3084792ecabcb0a77c6612f4772a0a3085cc5c
jenifernakabar <nakabar@gmail.com>
2022-03-29 21:17:02 -0300

    adicionando metodos que faltaram no swagger

--- src/main/java/br/com/gerenciador/veiculos/controller/VeiculoControllerDocs.java
+++ src/main/java/br/com/gerenciador/veiculos/controller/VeiculoControllerDocs.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.gerenciador.veiculos.dto.VeiculoDTO;
 import br.com.gerenciador.veiculos.entity.Veiculo;
@@ -25,25 +27,44 @@ public interface VeiculoControllerDocs {
 	            @ApiResponse(code = 400, message = "Campos informados inválidos") })
 	public VeiculoDTO criarVeiculo(@RequestBody VeiculoDTO veiculoDTO) throws VeiculoJaRegistradoException ;
 
-	@GetMapping
+	 @ApiOperation(value = "Listar todos os veiculos")
+	    @ApiResponses(value = {
+	    		@ApiResponse(code = 200, message = "Veiculo encontrado!"),
+	            @ApiResponse(code = 404, message = "Veiculo informado não encontrado") })
 	public ResponseEntity<List<Veiculo>> listar();
 
 	@ApiOperation(value = "Buscar um veiculo na base de dados")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Veiculo encontrado!"),
-            @ApiResponse(code = 400, message = "Veiculo informado não encontrado") })
+            @ApiResponse(code = 200, message = "Veiculo encontrado!"),
+            @ApiResponse(code = 404, message = "Veiculo informado não encontrado") })
 	public VeiculoDTO detalharVeiculo(@PathVariable Long id) throws VeiculoNaoEncontradoException;
 	
 	@ApiOperation(value = "Atualizar um veiculo na base de dados")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Veiculo atualizado com sucesso!"),
-            @ApiResponse(code = 400, message = "Veiculo informado não encontrado") })
+            @ApiResponse(code = 200, message = "Veiculo atualizado com sucesso!"),
+            @ApiResponse(code = 404, message = "Veiculo informado não encontrado") })
+	public ResponseEntity<VeiculoDTO> atualizarVeiculo(@PathVariable Long id,@RequestBody VeiculoDTO veiculoDTO) throws VeiculoNaoEncontradoException;
+	
+	@ApiOperation(value = "Atualizar o status do veiculo na base de dados")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Status do veiculo atualizado com sucesso!"),
+            @ApiResponse(code = 404, message = "Veiculo informado não encontrado") })
 	public VeiculoDTO atualizarStatus(@PathVariable Long id,@RequestBody VeiculoDTO veiculoDTO) throws VeiculoNaoEncontradoException;
 	
 	@ApiOperation(value = "Deletar um veiculo na base de dados")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Veiculo deletado com sucesso!"),
-            @ApiResponse(code = 400, message = "Veiculo informado não encontrado") })
+            @ApiResponse(code = 204, message = "Veiculo deletado com sucesso!"),
+            @ApiResponse(code = 404, message = "Veiculo informado não encontrado") })
 	public VeiculoDTO delecaoLogica(@PathVariable Long id) throws VeiculoNaoEncontradoException ;
+	
+	@ApiOperation(value = "Realizar Busca Dinamica")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Veiculo encontrado!"),
+            @ApiResponse(code = 400, message = "Veiculo informado não encontrado") })
+	public ResponseEntity<List<VeiculoDTO>> buscaDinamica(
+			@RequestParam(value ="fabricante",required = false, defaultValue="")String fabricante,
+			@RequestParam(value ="nome",required = false, defaultValue="") String nome,
+			@RequestParam(value ="ano",required = false, defaultValue="0") int ano) throws VeiculoNaoEncontradoException;
+   
 
 }
